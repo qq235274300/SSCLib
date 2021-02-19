c8c552dacac103342bdedc55df68a2844050dffb
Shaolongjie <shaolongjie@51hitech.com>
2021-02-19 13:52:57 +0800

    Delete Gizmo Folder

---
+++ Plugins/SSCPlugins/Source/SSCPlugins/Public/SSCUI/SSCPanelWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SSCObject/SSCUserWidget.h"
+#include "SSCPanelWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SSCPLUGINS_API USSCPanelWidget : public USSCUserWidget
+{
+	GENERATED_BODY()
+	
+};
